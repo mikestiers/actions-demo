bb5390ebb4f758c7b3bafbe1c37367fbec04685a
mikestiers <mike.stiers@gmail.com>
2020-05-06 20:13:53 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+    version = "~>2.60"
+}
+
+resource "azurerm_virtual_network" "hub-vnet" {
+  name                = "demo-vnet"
+  location            = "CentralUS"
+  resource_group_name = "contoso"
+  address_space       = ["10.0.0.0/16"]
+
+  tags {
+    environment = "demo"
+  }
+}
