f5efee988f1891f3375002cac9c6f149f741f9ad
mikestiers <mike.stiers@gmail.com>
2020-05-06 20:20:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,4 @@ resource "azurerm_virtual_network" "hub-vnet" {
   location            = "CentralUS"
   resource_group_name = "contoso"
   address_space       = ["10.0.0.0/16"]
-
-  tags {
-    environment = "demo"
-  }
 }
