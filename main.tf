52839bf379a1346d11df69671c77c7c2d548e95c
mikestiers <mike.stiers@gmail.com>
2020-05-07 11:16:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 provider "azurerm" {
-    version = "~>2.6"
-    tenant_id       = var.tenant_id
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    features {}
+  version         = "1.38.0"
+  subscription_id = var.sub
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
-resource "azurerm_virtual_network" "hub-vnet" {
-  name                = "demo-vnet"
-  location            = "CentralUS"
-  resource_group_name = "contoso"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_resource_group" "DevRG" {
+  name     = "Dev10"
+  location = "eastus"
+
+  tags = {
+    environment = "Dev"
+  }
 }
