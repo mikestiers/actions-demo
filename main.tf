c114dbb6a13ea8be595d00b4f3cafc66a626980e
Mike Stiers <mike.stiers@gmail.com>
2020-05-06 23:34:04 -0400

    adding sub stuff

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
     version = "~>2.6"
+    tenant_id       = var.tenant_id
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
     features {}
 }
 
